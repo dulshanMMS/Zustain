7c36a1f3434e8b2f24d25afdae0daf5308183cfb
dulshanMMS <168272579+dulshanMMS@users.noreply.github.com>
2025-11-23 21:46:34 +0530

    fix module resolution and typescript config

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { Button } from './Button';
+export { Input } from './Input';
+export { ExerciseCard } from './ExerciseCard';
+export { Loading } from './Loading';
+export { ErrorView } from './ErrorView';
